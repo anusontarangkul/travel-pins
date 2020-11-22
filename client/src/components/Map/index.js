345b4873e433425551e819dff21c8edaf7b4af37
Aaron Diggdon <aarondiggdon@gmail.com>
2020-11-22 15:27:07 -0800

    Added map styles

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -1,25 +1,3 @@
-// import React from "react";
-// import "./style.css";
-
-// var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
- 
-// mapboxgl.accessToken = 'pk.eyJ1IjoiYWFyb25kaWciLCJhIjoiY2tobnMzNnYwMDR1ejM1bzU1b2cyNnljNCJ9.qtupNlfb7Jam-q_Gdg0z2Q';
-// var map = new mapboxgl.Map({
-// container: 'map',
-// style: 'mapbox://styles/mapbox/streets-v11'
-// });
-
-// function Map(props) {
-//     return <div className="map" id="map">
-//         {map}
-//     </div>;
-//   }
-  
-//   export default Map;
-  
-
-
-  /* src/App.js */
 import React, { useRef, useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
 
@@ -34,10 +12,9 @@ const Map = () => {
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
-      // See style options here: https://docs.mapbox.com/api/maps/#styles
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-104.9876, 39.7405],
-      zoom: 12.5,
+      style: 'mapbox://styles/aarondig/ckhns66tw0zj71apj50vubwrq',
+      center: [-40.9876, 14.7405],
+      zoom: .4,
     });
 
     // add navigation control (the +/- zoom buttons)
