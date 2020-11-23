48ec917321c627ed853dd0041588671bec9497a4
Aaron Diggdon <aarondiggdon@gmail.com>
2020-11-22 16:58:14 -0800

    Learning to use reactIcons

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,12 +1,18 @@
 import react from 'react';
 import './style.css';
+import { IconContext } from "react-icons";
+import { MdHome } from "react-icons/md";
 
 function Navbar(){
     return <div className="navbar">
-        <div className="nav-item">Home</div>
-        <div className="nav-item">Map</div>
-        <div className="nav-item">Profile</div>
-        <div className="nav-item">Profile</div>
+        <IconContext.Provider value={{ style: {fontSize: '30px', color: "rgb(0, 123, 255)"}}}>
+            <div className="nav-item">
+                <MdHome />
+            </div>
+        </IconContext.Provider>
+            <div className="nav-item">Map</div>
+            <div className="nav-item">Profile</div>
+            <div className="nav-item">Profile</div>
     </div> ;
 
 }
