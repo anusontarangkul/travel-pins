b1cb0f12dec98699f03f1df7f538cef45a21df6c
Aaron Diggdon <aarondiggdon@gmail.com>
2020-11-23 15:48:32 -0800

    Merge with main

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 import Wrapper from './components/Wrapper';
-import Navbar from './components/Navbar'
-import Map from './components/Map'
+import Navbar from './components/Navbar';
+import Map from './components/Map';
+import Landing from './components/Landing'
 
 function App() {
   return (
