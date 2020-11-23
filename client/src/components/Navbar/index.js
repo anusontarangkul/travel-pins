85995ff57f93b128d3b1cb99ce4381bbbe486624
Aaron Diggdon <aarondiggdon@gmail.com>
2020-11-22 17:12:32 -0800

    Designed navbar and added icons

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,18 +1,22 @@
 import react from 'react';
 import './style.css';
-import { IconContext } from "react-icons";
 import { MdHome } from "react-icons/md";
+import { FiMapPin, FiHome, FiUser, FiMenu  } from "react-icons/fi";
 
 function Navbar(){
     return <div className="navbar">
-        <IconContext.Provider value={{ style: {fontSize: '30px', color: "rgb(0, 123, 255)"}}}>
             <div className="nav-item">
-                <MdHome />
+                <FiHome />
             </div>
-        </IconContext.Provider>
-            <div className="nav-item">Map</div>
-            <div className="nav-item">Profile</div>
-            <div className="nav-item">Profile</div>
+            <div className="nav-item">
+                <FiMapPin/>
+            </div>
+            <div className="nav-item">
+                <FiUser/>
+            </div>
+            <div className="nav-item">
+                <FiMenu/>
+            </div>
     </div> ;
 
 }
