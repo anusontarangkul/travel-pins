65d26921f0a9164851b81080005ad9cbdb66e9cf
Aaron Diggdon <aarondiggdon@gmail.com>
2020-11-23 18:22:31 -0800

    Adding styles to landing

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./style.css";
 
+
 function Landing(props) {
     return <div className="landing" {...props}>
       <div className="mainHeader">
         <h1 className="title">travel pins</h1>
         <h1 className="space"></h1>
       </div>
-      <button className="begin">Start</button>
+      <div className="iconContainer">
+        
+      </div>
+      <div className="iconInner">
+        
+      </div>
+      <i className="material-icons material-icons-outlined"id="pinButton">room</i>
     </div>;
   }
   
