370c0bd0d541abd5bfd7b95f2e63ceaecf7d81eb
Jonathan Honda <hondahelix120@gmail.com>
2020-11-24 11:21:05 -0800

    fronted login code

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,13 +1,27 @@
-import react from "react";
+import react, {useState , useRef} from "react";
+import API from "../../utils/API";
+
 
 function Login (){
+
+    const [loginState, setLogin] = useState({});
+    const userRef = useRef();
+    const passRef = useRef();
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        console.log(userRef.current.value);
+        console.log(passRef.current.value);
+    }
+
     return (
         <form>
             <h2>login</h2>
             <h5>username</h5>
-            <input id="username" name="username" autocomplete="off" class="input" type="text" ></input>
+            <input id="username" name="username" autocomplete="off" className="input" type="text" ref = {userRef}></input>
             <h5>password</h5>
-            <input id="password" name="password" autocomplete="off" class="input" type="password"/>
+            <input id="password" name="password" autocomplete="off" className="input" type="password" ref = {passRef}/>
+            <button type="button" onClick={handleLogin}>Login</button>
         </form>
     )
 }
--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -5,15 +5,15 @@ function Signup (){
         <form >
             <h2>Signup</h2>
             <h5>firstname</h5>
-            <input id="firstname" name="firstname" autocomplete="off" class="input" type="text" />
+            <input id="firstname" name="firstname" autocomplete="off" className="input" type="text" />
             <h5>last name</h5>
-            <input id="lastname" name="lastname" autocomplete="off" class="input" type="password"/>
+            <input id="lastname" name="lastname" autocomplete="off" className="input" type="password"/>
             <h5>username</h5>
-            <input id="username" name="username" autocomplete="off" class="input" type="text" />
+            <input id="username" name="username" autocomplete="off" className="input" type="text" />
             <h5>email</h5>
-            <input id="email" name="email" autocomplete="off" class="input" type="text" />
+            <input id="email" name="email" autocomplete="off" className="input" type="text" />
             <h5>password</h5>
-            <input id="password" name="password" autocomplete="off" class="input" type="password"/>
+            <input id="password" name="password" autocomplete="off" className="input" type="password"/>
         </form>
     )
 }
