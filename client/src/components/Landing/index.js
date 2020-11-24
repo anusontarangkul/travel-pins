84f15598671673b1d9064a8fa0fa28d59f42fbf7
Aaron Diggdon <aarondiggdon@gmail.com>
2020-11-23 22:19:30 -0800

    Added sphere on landing page

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -1,21 +1,26 @@
 import React from "react";
 import "./style.css";
 
-
 function Landing(props) {
-    return <div className="landing" {...props}>
+  return (
+    <div className="landing" {...props}>
       <div className="mainHeader">
         <h1 className="title">travel pins</h1>
         <h1 className="space"></h1>
       </div>
-      <div className="iconContainer">
-        
+      {/* <div className="iconContainer">
+        <i className="material-icons material-icons-outlined" id="pinButton">
+          room
+        </i>
+      </div> */}
+      <div className="sphere">
+        <div id="line1" />
+        <div id="line2" />
+        <div id="line3" />
+        <div id="line4" />
       </div>
-      <div className="iconInner">
-        
-      </div>
-      <i className="material-icons material-icons-outlined"id="pinButton">room</i>
-    </div>;
-  }
-  
-  export default Landing;
+    </div>
+  );
+}
+
+export default Landing;
