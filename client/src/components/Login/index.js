10a7ce8ea06f9d5093d8db128c665fbff492cab1
Aaron Diggdon <aarondiggdon@gmail.com>
2020-11-28 16:59:22 -0800

    Added to login styles

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -12,9 +12,10 @@ function Login (){
                     <h2 className="subhead">Sharing the world.</h2>
                 </div>
             </div>
+
             <div className="signupC container">
                 <div className="signupCard animated animatedFadeInUp fadeInUp">
-                    <h1 id="signupTitle">Signup</h1>
+                    <h1 className="formTitle">Signup</h1>
                     <form>
                         <h5>username</h5>
                         <input id="username" name="username" autocomplete="off" class="input" type="text" ></input>
@@ -25,17 +26,16 @@ function Login (){
             </div>
 
 
-
             <div className="loginC container">
-            <div className="loginCard animatedS animatedFadeInUp fadeInUp">
-                <h1 id="loginTitle">LOGIN</h1>
-                <form>
-                    <h5>username</h5>
-                    <input id="username" name="username" autocomplete="off" class="input" type="text" ></input>
-                    <h5>password</h5>
-                    <input id="password" name="password" autocomplete="off" class="input" type="password"/>
-                </form>
-            </div>
+                <div className="loginCard animatedS animatedFadeInUp fadeInUp">
+                    <h1 className="formTitle active">Login</h1>
+                    <form>
+                        <h5>username</h5>
+                        <input id="username" name="username" autocomplete="off" class="input" type="text" ></input>
+                        <h5>password</h5>
+                        <input id="password" name="password" autocomplete="off" class="input" type="password"/>
+                    </form>
+                </div>
             </div>
             
         </div>
