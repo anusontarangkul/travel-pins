f2270c4ca76830f45a04f42c13aa9a0a151348b9
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-12-01 14:51:16 -0800

    update format of login inputs

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -29,7 +29,7 @@ function Login() {
     const handleSignup = (event) => {
         event.preventDefault();
         //console.log(signupState);
-        API.userSignup(JSON.stringify({signupState}))
+        API.userSignup(JSON.stringify({ signupState }))
             .then(res => {
                 console.log("sign up success");
                 window.location.href = "/login";
@@ -60,14 +60,31 @@ function Login() {
                 <div className="signupCard animated animatedFadeInUp fadeInUp">
                     <h1 className="formTitle">Sign Up</h1>
                     <form>
-                        <h5>username</h5>
-                        <input id="username" name="username" autocomplete="off" class="input" type="text" onChange={(e) => handleSignupChange(e)} />
-
-                        <h5>email</h5>
-                        <input id="email" name="email" autocomplete="off" className="input" type="text" onChange={(e) => handleSignupChange(e)} />
-                        <h5>password</h5>
-                        <input id="password" name="password" autocomplete="off" class="input" type="password" onChange={(e) => handleSignupChange(e)} />
-                        <button type="button" onClick={handleSignup}>Sign Up</button>
+                        {/* <h5>First Name</h5> */}
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <input placeholder="&nbsp;First Name" id="firstName" name="username" autocomplete="off" class="input" type="text" onChange={(e) => handleSignupChange(e)} />
+                        {/* <h5>Last Name</h5> */}
+                        <br></br>
+                        <br></br>
+                        <input placeholder="&nbsp;Last Name" id="lastName" name="username" autocomplete="off" class="input" type="text" onChange={(e) => handleSignupChange(e)} />
+                        <br></br>
+                        <br></br>
+                        {/* <h5>username</h5> */}
+                        <input placeholder="&nbsp;Username" id="username" name="username" autocomplete="off" class="input" type="text" onChange={(e) => handleSignupChange(e)} />
+                        <br></br>
+                        <br></br>
+                        {/* <h5>email</h5> */}
+                        <input placeholder="&nbsp;Email" id="email" name="email" autocomplete="off" className="input" type="text" onChange={(e) => handleSignupChange(e)} />
+                        {/* <h5>password</h5> */}
+                        <br></br>
+                        <br></br>
+                        <input placeholder="&nbsp;Password" id="password" name="password" autocomplete="off" class="input" type="password" onChange={(e) => handleSignupChange(e)} />
+                        <br></br>
+                        <br></br>
+                        <button class="submit-btn" type="button" onClick={handleSignup}>Sign Up</button>
                     </form>
                 </div>
             </div>
@@ -75,13 +92,17 @@ function Login() {
 
             <div className="loginC container">
                 <div className="loginCard animatedS animatedFadeInUp fadeInUp">
-                    <h1 className="formTitle active">Sign In</h1>
+                    <h1 className="formTitle active" >Sign In</h1>
                     <form>
-                        <h5>username</h5>
-                        <input id="username" name="username" autocomplete="off" class="input" type="text" onChange={(e) => handleLoginChange(e)}></input>
-                        <h5>password</h5>
-                        <input id="password" name="password" autocomplete="off" class="input" type="password" onChange={(e) => handleLoginChange(e)}/>
-                        <button type="button" onClick={handleLogin}>Sign In</button>
+                        {/* <h5>Username</h5> */}
+                        <input placeholder="&nbsp;Username" id="username" name="username" autocomplete="off" class="input" type="text" onChange={(e) => handleLoginChange(e)}></input>
+                        {/* <h5>Password</h5> */}
+                        <br></br>
+                        <br></br>
+                        <input placeholder="&nbsp;Password" id="password" name="password" autocomplete="off" class="input" type="password" onChange={(e) => handleLoginChange(e)} />
+                        <br></br>
+                        <br></br>
+                        <button type="button" class="submit-btn" onClick={handleLogin}>Sign In</button>
                     </form>
                 </div>
             </div>
